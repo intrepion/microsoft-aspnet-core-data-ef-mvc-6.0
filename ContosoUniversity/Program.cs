637e1a570918b15bb59df86f5478c7d7f612c9b2
Oliver Forral <intrepion@gmail.com>
2023-08-31 22:03:38 -0600

    Initialize DB with test data.

--- ContosoUniversity/Program.cs
+++ ContosoUniversity/Program.cs
@@ -1,6 +1,6 @@
+using ContosoUniversity.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
-using ContosoUniversity.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -18,6 +18,21 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+}
+else
+{
+    app.UseDeveloperExceptionPage();
+    app.UseMigrationsEndPoint();
+}
+
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+    var context = services.GetRequiredService<SchoolContext>();
+    context.Database.EnsureCreated();
+    DbInitializer.Initialize(context);
 }
 
 app.UseHttpsRedirection();
